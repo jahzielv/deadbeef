f20ce27f33e4ea22d8e2404c02fbfa66426b21ca
Jahziel Villasana <jahzielv@gmail.com>
2019-12-30 17:35:21 -0500

    added octal support

--- src/components/ConvForm.js
+++ src/components/ConvForm.js
@@ -4,10 +4,16 @@ import "../App.css";
 
 const dec2hex = anyBase(anyBase.DEC, anyBase.HEX);
 const dec2bin = anyBase(anyBase.DEC, anyBase.BIN);
+const dec2oct = anyBase(anyBase.DEC, anyBase.OCT);
 const hex2dec = anyBase(anyBase.HEX, anyBase.DEC);
 const hex2bin = anyBase(anyBase.HEX, anyBase.BIN);
+const hex2oct = anyBase(anyBase.HEX, anyBase.OCT);
 const bin2dec = anyBase(anyBase.BIN, anyBase.DEC);
 const bin2hex = anyBase(anyBase.BIN, anyBase.HEX);
+const bin2oct = anyBase(anyBase.BIN, anyBase.OCT);
+const oct2dec = anyBase(anyBase.OCT, anyBase.DEC);
+const oct2bin = anyBase(anyBase.OCT, anyBase.BIN);
+const oct2hex = anyBase(anyBase.OCT, anyBase.HEX);
 
 class ConvForm extends Component {
     constructor(props) {
@@ -17,43 +23,46 @@ class ConvForm extends Component {
             decdata: "decimal",
             hexdata: "hexadecimal",
             bindata: "binary",
+            octdata: "octal",
             currConv: []
         };
     }
 
     handleDecChange = e => {
-        // console.log("yeet");
         if (this.decInputCheck(e.target.value)) {
             if (e.target.value === "") {
                 this.setState({
                     decdata: "decimal",
                     hexdata: "hexadecimal", //dec2hex(e.target.value),
-                    bindata: "binary" //dec2bin(e.target.value)
+                    bindata: "binary", //dec2bin(e.target.value)
+                    octdata: "octal"
                 });
             } else {
                 this.setState({
                     decdata: e.target.value,
                     hexdata: this.state.currConv[0](e.target.value), //dec2hex(e.target.value),
-                    bindata: this.state.currConv[1](e.target.value) //dec2bin(e.target.value)
+                    bindata: this.state.currConv[1](e.target.value), //dec2bin(e.target.value)
+                    octdata: this.state.currConv[2](e.target.value) //dec2hex(e.target.value),
                 });
             }
         }
     };
 
     handleHexChange = e => {
-        // console.log("yeet");
         if (this.hexInputCheck(e.target.value)) {
             if (e.target.value === "") {
                 this.setState({
                     decdata: "decimal",
                     hexdata: "hexadecimal", //dec2hex(e.target.value),
-                    bindata: "binary" //dec2bin(e.target.value)
+                    bindata: "binary", //dec2bin(e.target.value)
+                    octdata: "octal"
                 });
             } else {
                 this.setState({
                     decdata: this.state.currConv[0](e.target.value), //dec2hex(e.target.value),
                     hexdata: e.target.value,
-                    bindata: this.state.currConv[1](e.target.value) //dec2bin(e.target.value)
+                    bindata: this.state.currConv[1](e.target.value), //dec2bin(e.target.value)
+                    octdata: this.state.currConv[2](e.target.value) //dec2hex(e.target.value),
                 });
             }
         } else {
@@ -62,18 +71,19 @@ class ConvForm extends Component {
     };
 
     handleBinChange = e => {
-        // console.log("yeet");
         if (this.binaryInputCheck(e.target.value)) {
             if (e.target.value === "") {
                 this.setState({
                     decdata: "decimal",
                     hexdata: "hexadecimal", //dec2hex(e.target.value),
-                    bindata: "binary" //dec2bin(e.target.value)
+                    bindata: "binary", //dec2bin(e.target.value),
+                    octdata: "octal"
                 });
             } else {
                 this.setState({
                     decdata: this.state.currConv[0](e.target.value), //dec2bin(e.target.value)
                     hexdata: this.state.currConv[1](e.target.value), //dec2hex(e.target.value),
+                    octdata: this.state.currConv[2](e.target.value), //dec2hex(e.target.value),
                     bindata: e.target.value
                 });
             }
@@ -82,27 +92,57 @@ class ConvForm extends Component {
         }
     };
 
+    handleOctalChange = e => {
+        if (this.octalInputCheck(e.target.value)) {
+            if (e.target.value === "") {
+                this.setState({
+                    decdata: "decimal",
+                    hexdata: "hexadecimal", //dec2hex(e.target.value),
+                    bindata: "binary", //dec2bin(e.target.value),
+                    octdata: "octal"
+                });
+            } else {
+                this.setState({
+                    decdata: this.state.currConv[0](e.target.value), //dec2bin(e.target.value)
+                    hexdata: this.state.currConv[1](e.target.value), //dec2hex(e.target.value),
+                    bindata: this.state.currConv[2](e.target.value),
+                    octdata: e.target.value
+                });
+            }
+        } else {
+            console.log("octal decimal only!");
+        }
+    };
+
     handleDecClick = e => {
         if (this.state.decdata === "decimal") {
-            this.setState({ decdata: "", currConv: [dec2hex, dec2bin] });
+            this.setState({ decdata: "", currConv: [dec2hex, dec2bin, dec2oct] });
         } else {
-            this.setState({ currConv: [dec2hex, dec2bin] });
+            this.setState({ currConv: [dec2hex, dec2bin, dec2oct] });
         }
     };
 
     handleHexClick = e => {
         if (this.state.hexdata === "hexadecimal") {
-            this.setState({ hexdata: "", currConv: [hex2dec, hex2bin] });
+            this.setState({ hexdata: "", currConv: [hex2dec, hex2bin, hex2oct] });
         } else {
-            this.setState({ currConv: [hex2dec, hex2bin] });
+            this.setState({ currConv: [hex2dec, hex2bin, hex2oct] });
         }
     };
 
     handleBinClick = e => {
         if (this.state.bindata === "binary") {
-            this.setState({ bindata: "", currConv: [bin2dec, bin2hex] });
+            this.setState({ bindata: "", currConv: [bin2dec, bin2hex, bin2oct] });
         } else {
-            this.setState({ currConv: [bin2dec, bin2hex] });
+            this.setState({ currConv: [bin2dec, bin2hex, bin2oct] });
+        }
+    };
+
+    handleOctClick = e => {
+        if (this.state.octdata === "octal") {
+            this.setState({ octdata: "", currConv: [oct2dec, oct2hex, oct2bin] });
+        } else {
+            this.setState({ currConv: [oct2dec, oct2hex, oct2bin] });
         }
     };
 
@@ -124,6 +164,12 @@ class ConvForm extends Component {
         }
     };
 
+    focusOutOct = e => {
+        if (this.state.octdata === "") {
+            this.setState({ octdata: "octal" });
+        }
+    };
+
     decInputCheck = str => {
         return /^(\d*)?\d+$/.test(str) || str === "";
     };
@@ -134,6 +180,10 @@ class ConvForm extends Component {
 
     binaryInputCheck = str => {
         return /^[0-1]{1,}$/.test(str) || str === "";
+    };
+
+    octalInputCheck = str => {
+        return /^([0-7]*\.)?[0-7]+$/.test(str) || str === "";
     };
 
     render() {
@@ -166,6 +216,14 @@ class ConvForm extends Component {
                         onChange={this.handleBinChange}
                         onClick={this.handleBinClick}
                         onBlur={this.focusOutBin}
+                    />
+                    <input
+                        type="text"
+                        className="input"
+                        value={this.state.octdata}
+                        onChange={this.handleOctalChange}
+                        onClick={this.handleOctClick}
+                        onBlur={this.focusOutOct}
                     />
                 </form>
             </React.Fragment>
